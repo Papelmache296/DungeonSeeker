cb4822258003574cadff8f583cf573351f211a73
Papelmache296 <2024296@student.cct.ie>
2025-11-13 20:05:00 +0000

    Random created
    Random created in the spawnEnemy class to handle the spawing of each monster

--- Seeker/src/main/java/com/wololo/seeker/enemySpawner.java
+++ Seeker/src/main/java/com/wololo/seeker/enemySpawner.java
@@ -4,12 +4,50 @@
  */
 package com.wololo.seeker;
 
+import java.util.Random;
+
 /**
  *
  * @author sanch
  */
-public class enemySpawner {
+public class EnemySpawner {
     
+    public Enemy spawnEnemy(){
+        
+        Enemy enemy = null;
+        
+    Random spawnMonster = new Random();
+            
+            int spawn = spawnMonster.nextInt(100);
+            int odds = 0;
+            //40% odds of spawing a Goblin
+            odds += 40;
+            if(spawn < odds){
+            enemy = spawnGoblin();                                            
+}
+            //25% odds of spawing a Zombie
+            odds += 25;
+            if(spawn < odds){
+            enemy = spawnZombie();                       
+}
+            //25% odds of spawing a Skeleton
+            odds += 25;
+            if(spawn < odds){
+            enemy = spawnSkeleton();            
+}
+            //10% odds of spawing a Troll
+            odds += 10;
+            if(spawn <odds){
+            enemy = spawnTroll();                       
+}
+            return enemy;
+            assert odds ==100;
+    }
+}    
+            
+
+
+            
     
     /*
     This class will hold all related to enemy spawning:
@@ -18,4 +56,5 @@ public class enemySpawner {
     - probability of spawning to not get many high level enemies in one room
     -
     */
-}
+
+
