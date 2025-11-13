2a4ee7ca7e4a9b440f8f155132d9bd2d97faf54d
Papelmache296 <2024296@student.cct.ie>
2025-11-13 16:15:29 +0000

    new class for enemies
    enemySpawner added to handle all related to monster spawing.

---
+++ Seeker/src/main/java/com/wololo/seeker/enemySpawner.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.wololo.seeker;
+
+/**
+ *
+ * @author sanch
+ */
+public class enemySpawner {
+    
+    
+    /*
+    This class will hold all related to enemy spawning:
+    we need:
+    - Randomizer to get monsters from 1-6
+    - probability of spawning to not get many high level enemies in one room
+    -
+    */
+}
